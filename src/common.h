d21e1669531dcdb9fe8c2e0c9018bfad662d6ea7
lindeneg <christian@lindeneg.org>
2025-07-30 19:39:11 +0200

    cleanup asserts

--- src/common.h
+++ src/common.h
@@ -21,80 +21,61 @@ typedef double f64;
 
 typedef unsigned char uchar;
 
-#define RADIANS(deg) (deg * constants::PI / 180.f)
-#define DEGREES(rad) (rad * 180.f / constants::PI)
-
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
-
-#define ABS(x) ((x) < 0 ? -(x) : (x))
-#define CLAMP(x, min, max) (MAX((min), MIN((max), (x))))
-
-internal const float REL_EPSILON{1e-8f};
-internal const float ABS_EPSILON{1e-12f};
-#define EQUAL_REL(a, b) (ABS((a) - (b)) <= MAX(ABS(a), ABS(b)) * REL_EPSILON)
-#define EQUAL(a, b) (ABS((a) - (b)) <= ABS_EPSILON || EQUAL_REL((a), (b)))
-
-#define GREATER_OR_EQUAL(a, b) (EQUAL((a), (b)) || (a) > (b))
-#define LESS_OR_EQUAL(a, b) (EQUAL((a), (b)) || (a) < (b))
-#define GREATER(a, b) (!EQUAL((a), (b)) && (a) > (b))
-#define LESSER(a, b) (!EQUAL((a), (b)) && (a) < (b))
-
-#define LOG_ERROR(fmt, ...)                                                  \
-    fprintf(stderr, "[ERROR] [%s:%d:%s] " fmt, __FILE__, __LINE__, __func__, \
-            ##__VA_ARGS__);
-
-#define LOG_WARN(fmt, ...)                                                   \
-    fprintf(stderr, "[WARN]  [%s:%d:%s] " fmt, __FILE__, __LINE__, __func__, \
-            ##__VA_ARGS__);
-
-#define ASSERT(expr)                                                       \
-    do {                                                                   \
-        if (!(expr)) {                                                     \
-            LOG_ERROR("[ASSERT] Fatal error: Expression '%s' was false\n", \
-                      #expr);                                              \
-            std::abort();                                                  \
-        }                                                                  \
+#define ASSERT(expr)                                                         \
+    do {                                                                     \
+        if (!(expr)) {                                                       \
+            fprintf(stderr,                                                  \
+                    "[ASSERT ERROR] [%s:%d:%s] Expression '%s' was false\n", \
+                    __FILE__, __LINE__, __func__, #expr);                    \
+            std::abort();                                                    \
+        }                                                                    \
     } while (0)
 
-#define ASSERT_MSG(expr, fmt, ...)                                  \
-    do {                                                            \
-        if (!(expr)) {                                              \
-            LOG_ERROR("[ASSERT] Fatal error: " fmt, ##__VA_ARGS__); \
-            std::abort();                                           \
-        }                                                           \
+#define ASSERT_MSG(expr, fmt, ...)                                           \
+    do {                                                                     \
+        if (!(expr)) {                                                       \
+            fprintf(stderr, "[ASSERT ERROR] [%s:%d:%s] " fmt "\n", __FILE__, \
+                    __LINE__, __func__, ##__VA_ARGS__);                      \
+            std::abort();                                                    \
+        }                                                                    \
     } while (0)
 
-#define ASSERT_RET(expr, ret)                                        \
-    do {                                                             \
-        if (!(expr)) {                                               \
-            LOG_WARN("[ASSERT] Expression '%s' was false\n", #expr); \
-            return (ret);                                            \
-        }                                                            \
+#define ASSERT_RET(expr, ret)                                                  \
+    do {                                                                       \
+        if (!(expr)) {                                                         \
+            fprintf(stderr,                                                    \
+                    "[ASSERT WARNING] [%s:%d:%s] Expression '%s' was false\n", \
+                    __FILE__, __LINE__, __func__, #expr);                      \
+            return (ret);                                                      \
+        }                                                                      \
     } while (0)
 
-#define ASSERT_RET_V(expr)                                           \
-    do {                                                             \
-        if (!(expr)) {                                               \
-            LOG_WARN("[ASSERT] Expression '%s' was false\n", #expr); \
-            return;                                                  \
-        }                                                            \
+#define ASSERT_RET_V(expr)                                                     \
+    do {                                                                       \
+        if (!(expr)) {                                                         \
+            fprintf(stderr,                                                    \
+                    "[ASSERT WARNING] [%s:%d:%s] Expression '%s' was false\n", \
+                    __FILE__, __LINE__, __func__, #expr);                      \
+            return;                                                            \
+        }                                                                      \
     } while (0)
 
-#define ASSERT_RET_MSG(expr, ret, fmt, ...)           \
-    do {                                              \
-        if (!(expr)) {                                \
-            LOG_WARN("[ASSERT] " fmt, ##__VA_ARGS__); \
-            return (ret);                             \
-        }                                             \
+#define ASSERT_RET_MSG(expr, ret, fmt, ...)                                    \
+    do {                                                                       \
+        if (!(expr)) {                                                         \
+            fprintf(stderr, "[ASSERT WARNING] [%s:%d:%s] " fmt "\n", __FILE__, \
+                    __LINE__, __func__, ##__VA_ARGS__);                        \
+            return (ret);                                                      \
+        }                                                                      \
     } while (0)
 
-#define ASSERT_RET_V_MSG(expr, fmt, ...)              \
-    do {                                              \
-        if (!(expr)) {                                \
-            LOG_WARN("[ASSERT] " fmt, ##__VA_ARGS__); \
-            return;                                   \
-        }                                             \
+#define ASSERT_RET_V_MSG(expr, fmt, ...)                                       \
+    do {                                                                       \
+        if (!(expr)) {                                                         \
+            fprintf(stderr, "[ASSERT WARNING] [%s:%d:%s] " fmt "\n", __FILE__, \
+                    __LINE__, __func__, ##__VA_ARGS__);                        \
+            return;                                                            \
+        }                                                                      \
     } while (0)
 
 namespace explore {
