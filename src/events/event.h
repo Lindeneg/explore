b104869730f7b275bc87cb7abfb7b2b0faa9865e
lindeneg <christian@lindeneg.org>
2025-11-03 03:30:38 +0100

    base event class

---
+++ src/events/event.h
@@ -0,0 +1,11 @@
+#ifndef EXPLORE_EVENTS_EVENT_H_
+#define EXPLORE_EVENTS_EVENT_H_
+
+namespace explore::event {
+class Event {
+   public:
+    Event() = default;
+};
+}  // namespace explore::event
+
+#endif  // EXPLORE_EVENTS_EVENT_H_
