056bf799dedf381344d189f95d794619bbd25a22
lindeneg <christian@lindeneg.org>
2025-11-03 02:30:26 +0100

    event collision

---
+++ src/events/collision.h
@@ -0,0 +1,17 @@
+#ifndef EXPLORE_EVENTS_COLLISION_H_
+#define EXPLORE_EVENTS_COLLISION_H_
+
+#include "../ecs/ecs.h"
+
+namespace explore::event {
+
+struct Collision : public Event {
+    ecs::Entity a;
+    ecs::Entity b;
+
+    Collision(ecs::Entity a, ecs::Entity b) : a(a), b(b) {}
+};
+
+}  // namespace explore::event
+
+#endif  // EXPLORE_EVENTS_COLLISION_H_
