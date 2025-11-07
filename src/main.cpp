b9a046c645a2de2dfc3d86a637789f0b86ba89b9
lindeneg <christian@lindeneg.org>
2025-11-07 06:51:18 +0100

    tidy up main

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,10 @@
 #include "./common.h"
 #include "./managers/game_manager.h"
 
-using namespace explore;
-
 i32 main() {
     // TODO read config file or use argument vector
     spdlog::set_level(spdlog::level::trace);
-    manager::GameManager game_manager{};
+    explore::manager::GameManager game_manager{};
     if (!game_manager.initialize()) {
         return EXIT_FAILURE;
     }
