e330f1eea8ffa102d468f17c35cb85e8c9b3316b
lindeneg <christian@lindeneg.org>
2025-11-07 06:50:43 +0100

    add keyboard control component

--- src/ecs/components.h
+++ src/ecs/components.h
@@ -65,6 +65,22 @@ struct BoxCollider {
         : width(width), height(height), offset(offset) {}
 };
 
+struct KeyboardControl {
+    glm::vec2 up_velocity;
+    glm::vec2 right_velocity;
+    glm::vec2 down_velocity;
+    glm::vec2 left_velocity;
+
+    KeyboardControl(glm::vec2 up_velocity = glm::vec2(0),
+                    glm::vec2 right_velocity = glm::vec2(0),
+                    glm::vec2 down_velocity = glm::vec2(0),
+                    glm::vec2 left_velocity = glm::vec2(0))
+        : up_velocity(up_velocity),
+          right_velocity(right_velocity),
+          down_velocity(down_velocity),
+          left_velocity(left_velocity) {}
+};
+
 }  // namespace explore::component
 
 #endif  // EXPLORE_ECS_COMPONENTS_H_
