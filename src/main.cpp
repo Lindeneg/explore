28eee4af366103d4c12d3ce0a1d7baf94c47e0cc
lindeneg <christian@lindeneg.org>
2025-11-27 11:42:31 +0100

    take log level from cmdline

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
+#include <spdlog/common.h>
 #include <spdlog/spdlog.h>
+
+#include <string>
 
 #include "./common.h"
 #include "./managers/game_manager.h"
 
-i32 main() {
-    // TODO read config file or use argument vector
-    spdlog::set_level(spdlog::level::debug);
+i32 main(int argc, char **argv) {
+    auto log_level{spdlog::level::info};
+    for (int i = 0; i < argc; i++) {
+        std::string key{argv[i]};
+
+        if ((key == "--log-level" || key == "-L") && i + 1 < argc) {
+            log_level = spdlog::level::from_str(argv[i + 1]);
+            i++;
+        }
+    }
+
+    spdlog::set_level(log_level);
+    spdlog::info("log level: {}", spdlog::level::to_string_view(log_level));
+
     explore::manager::GameManager game_manager{};
     if (!game_manager.initialize()) {
         return EXIT_FAILURE;
